bad425fbd79f0170cf65d754d01f5c288beb5e24
jo87jimmy <jo87jimmy@gmail.com>
2025-06-05 15:29:05 +0800

    test: add unit tests for OCRDetect component

--- src/components/__tests__/OCRDetect.spec.ts
+++ src/components/__tests__/OCRDetect.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import OCRDetect from '../OCRDetect.vue'
 
-describe('HelloWorld', () => {
+describe('OCRDetect', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(OCRDetect, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
